996915f653106ff28eb0d47f84e85a4283d145ff
Puneet <puneetgupta18190@gmail.com>
2019-05-18 21:11:09 +0530

    added category, product, orders, order items
    - added bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+ #  rescue_from CanCan::AccessDenied do | exception |
+ #   redirect_to root_url, alert: exception.message
+ # end
+  helper_method :current_order
+  def current_order
+    if  !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
+
+  def user_is_admin
+    return if user_signed_in? && current_user.admin?
+
+    flash[:info] = "You don't have the privilages for this action!"
+    redirect_back(fallback_location: root_url)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  Roles = [ :admin , :default ]
+  # def is?( requested_role )
+  #   self.role == requested_role.to_s
+  # end
+
+  def admin?
+  	self.role == 'admin'
+  end
 end
